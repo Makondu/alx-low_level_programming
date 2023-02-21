9fdb812e8b8a7d51f984051e4dcc1a17b30d8885
Makondu <makonduchiyesu@gmail.com>
2023-02-21 09:18:48 -0800

    Fixing 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - Entry point
+ * main - prints _putchar
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+        char text[9] = "_putchar";
+
+        int i = 0;
+        for(i=0; i<8; i++);
+        {
+                _putchar(text[i]);
+        }
+        _putchar("\n");
+        return (0);
 }
