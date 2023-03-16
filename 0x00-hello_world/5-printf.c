fa15fc2217ebd64fc7ff8d0812804611be57bdd7
Makondu <makonduchiyesu@gmail.com>
2023-03-16 14:54:40 -0700

    Adding 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - program starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char _print1[56] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", _print1);
+
+	return (0);
+}
