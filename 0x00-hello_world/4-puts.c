a29f58b9f13a5061c1d6631bcbcc06faba0a5369
Makondu <makonduchiyesu@gmail.com>
2023-03-16 13:56:55 -0700

    Adding 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - program starts
+ *
+ * Return - Success is 0
+ */
+int main()
+{
+	char str[52];
+
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+
+	puts(str);
+
+	return (0);
+}
