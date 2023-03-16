a8189b3d69c1392740d88f41e63f50f7cdd497e7
Makondu <makonduchiyesu@gmail.com>
2023-03-16 15:41:46 -0700

    Adding 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - program starts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+        printf("Size of a long: %d byte(s)\n", sizeof(long));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
