83d6d44a509059bca5b6dfdc029a1ce0bcc76334
Makondu <makonduchiyesu@gmail.com>
2023-03-17 10:00:25 -0700

    Fixing 0-positive_or_negative.c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,21 +13,21 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX/2;
-	printf("%d\n", n);
+	printf("%d", n);
 
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf(" is positive\n");
 	}
 
 	if (n == 0)
 	{
-		printf("is zero\n");
+		printf(" is zero\n");
 	}
 
 	if (n < 0)
 	{
-		printf("is negative\n");
+		printf(" is negative\n");
 	}
 
 	return (0);
